5b11e083b4ba0c486fd384057129cab2d6dd536b
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2023-07-14 15:49:43 -0300

    Update models.py

--- powermeter/meters/models.py
+++ powermeter/meters/models.py
@@ -2,17 +2,24 @@ from django.db import models
 from model_utils.models import TimeStampedModel
 
 
-class Meter(TimeStampedModel):
-    meter_key = models.CharField(unique=True, max_length=200)
-    name = models.CharField(max_length=200)
+class Instrument(TimeStampedModel):
+    name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
-        return f"{self.meter_key}"
+        return f"{self.name}"
 
 
 class Measure(TimeStampedModel):
-    consumption = models.PositiveIntegerField()
-    meter = models.ForeignKey(Meter, related_name="measures", on_delete=models.CASCADE)
+    consumption = models.PositiveIntegerField(deafult=0)
+    instrument = models.ForeignKey(Instrument, related_name="measures", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Measure's meter {self.meter_id} consumption: {self.consumption}"
+        return f"Measure's Instrument {self.instrument_id} consumption: {self.consumption}"
+
+
+class Tag(TimeStampedModel):
+    name = models.CharField(unique=True, max_length=200)
+    instrument = models.ManyToManyField(Instrument, related_name="tags")
+
+    def __str__(self):
+        return f"Measure's Instrument {self.instrument_id} consumption: {self.consumption}"
